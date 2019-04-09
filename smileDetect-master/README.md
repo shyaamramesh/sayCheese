315e450ad7e8372f0172485e6251323773643bdc
Shyaam Ramesh <49405098+shyaamramesh@users.noreply.github.com>
2019-04-09 00:30:32 -0400

    Add files via upload

---
+++ smileDetect-master/README.md
@@ -0,0 +1,7 @@
+# Smiledetect
+A simple smile detection algorithm using openCV and haarCascades.
+<br> First using a frontalface cascade we detect the face => Then we use a smile cascade to detect smiles.
+<br> The smile cascade works by detecting mouth shapes and when eyes slightly squint.
+<br><br> Also if your wondering what a haar cascade is, well it's a huge XML file detailing the structure of something. Nowadays we use machine learning, so i am planning for a V2 using ML.
+<br>
+<i> To run the program make sure you clone the XML cascade files too ! Run the <code><u>sayCheese.py</u></code><i>
---
+++ smileDetect-master/sayCheese.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+#libs
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+#haar
+
+cap = cv2.VideoCapture(0) #0 is default camera change it per your needs
+#setup vidcap
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) #face
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #smile
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+        smile = smile_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in smile:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow('img',img) #show cap
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
